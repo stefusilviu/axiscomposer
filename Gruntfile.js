9a39f7dfd949ec8474cdb135a776807ad0eb997b
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-01-09 14:30:30 +0545

    let's start with initial backbone admin file

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,8 @@ module.exports = function( grunt ){
 				'!<%= dirs.scripts %>/admin/*.min.js',
 				'<%= dirs.scripts %>/modal/*.js',
 				'!<%= dirs.scripts %>/modal/*.min.js',
+				'<%= dirs.scripts %>/backbone/*.js',
+				'!<%= dirs.scripts %>/backbone/*.min.js',
 				'<%= dirs.scripts %>/frontend/*.js',
 				'!<%= dirs.scripts %>/frontend/*.min.js'
 			]
@@ -54,6 +56,18 @@ module.exports = function( grunt ){
 						'!*.min.js'
 					],
 					dest: '<%= dirs.scripts %>/modal/',
+					ext: '.min.js'
+				}]
+			},
+			backbone: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.scripts %>/backbone/',
+					src: [
+						'*.js',
+						'!*.min.js'
+					],
+					dest: '<%= dirs.scripts %>/backbone/',
 					ext: '.min.js'
 				}]
 			},
@@ -111,9 +125,11 @@ module.exports = function( grunt ){
 				files: [
 					'<%= dirs.scripts %>/admin/*.js',
 					'<%= dirs.scripts %>/modal/*.js',
+					'<%= dirs.scripts %>/backbone/*.js',
 					'<%= dirs.scripts %>/frontend/*.js',
 					'!<%= dirs.scripts %>/admin/*.min.js',
 					'!<%= dirs.scripts %>/modal/*.min.js',
+					'!<%= dirs.scripts %>/backbone/*.min.js',
 					'!<%= dirs.scripts %>/frontend/*.min.js'
 				],
 				tasks: ['uglify']
---
+++ assets/scripts/backbone/admin.js
@@ -0,0 +1,10 @@
+/* global jQuery, Backbone, _ */
+
+/**
+ * AxisBuilder Admin JS
+ */
+( function ( $, Backbone, _ ) {
+	'use strict';
+
+
+}( jQuery, Backbone, _ ));

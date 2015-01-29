670f0db7338ee5741166bef10b2d04be21a6e30d
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-01-29 20:26:55 +0545

    Admin footer text improvements

---
+++ includes/admin/builder-admin-functions.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * AxisFramework Admin Functions
+ *
+ * @package		AxisFramework/Admin/Functions
+ * @category	Core
+ * @author		AxisThemes
+ * @since		Version 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Get all AxisBuilder screen ids
+ * @return array
+ */
+function axisbuilder_get_screen_ids() {
+
+	$axis_screen_id = sanitize_title( __( 'Axis Builder', 'woocommerce' ) );
+	$screen_ids     = array(
+		'toplevel_page_' . $axis_screen_id,
+		$axis_screen_id . '_page_axisbuilder-iconfonts',
+		$axis_screen_id . '_page_axisbuilder-settings',
+		$axis_screen_id . '_page_axisbuilder-status',
+		$axis_screen_id . '_page_axisbuilder-addons'
+	);
+
+	return apply_filters( 'axisbuilder_screen_ids', $screen_ids );
+}
--- includes/admin/class-builder-admin.php
+++ includes/admin/class-builder-admin.php
@@ -25,13 +25,16 @@ class AB_Admin {
 	 */
 	public function __construct() {
 		add_action( 'init', array( $this, 'includes' ) );
-		add_filter( 'admin_footer_text', array( $this, 'admin_footer_text' ), 1, 2 );
+		add_filter( 'admin_footer_text', array( $this, 'admin_footer_text' ), 1 );
 	}
 
 	/**
 	 * Include any classes we need within admin.
 	 */
 	public function includes() {
+		// Functions
+		include_once( 'builder-admin-functions.php' );
+
 		// Classes
 		include_once( 'class-builder-admin-editor.php' );
 		include_once( 'class-builder-admin-meta-boxes.php' );
@@ -49,15 +52,17 @@ class AB_Admin {
 	}
 
 	/**
-	 * Change the admin footer text on AxisBuilder admin pages
+	 * Change the admin footer text on AxisBuilder admin pages.
+	 * @param  string $footer_text
 	 * @return string
 	 */
 	public function admin_footer_text( $footer_text ) {
-		$screen = get_current_screen();
+		$current_screen = get_current_screen();
 
 		// Check to make sure we're on a AxisBuilder admin page
-		if ( in_array( $screen->id, array( 'settings_page_axisbuilder-iconfonts' ) ) ) {
-			$footer_text = sprintf( __( 'Please rate <strong>AxisBuilder</strong> <a href="%1$s" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a> on <a href="%1$s" target="_blank">WordPress.org</a> to help us keep this plugin free. A huge thank you from AxisThemes!', 'axisbuilder' ), __( 'https://wordpress.org/support/view/plugin-reviews/axis-builder?filter=5', 'axisbuilder' ) );
+		if ( isset( $current_screen->id ) && apply_filters( 'axisbuilder_display_admin_footer_text', in_array( $current_screen->id, axisbuilder_get_screen_ids() ) ) ) {
+			// Change the footer text
+			$footer_text = sprintf( __( 'Please rate <strong>AxisBuilder</strong> <a href="%1$s" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a> on <a href="%1$s" target="_blank">WordPress.org</a> to help us keep this plugin free. A huge thank you from AxisThemes in advance!', 'axisbuilder' ), 'https://wordpress.org/support/view/plugin-reviews/axis-builder?filter=5' );
 		}
 
 		return $footer_text;

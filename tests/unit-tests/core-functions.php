01c002a39221c473671584aa09de1af143c5b7da
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-02-04 11:31:28 +0545

    Unit Test axisbuilder_clean()

--- tests/unit-tests/core-functions.php
+++ tests/unit-tests/core-functions.php
@@ -7,6 +7,17 @@
 class AB_Tests_Core_Functions extends AB_Unit_Test_Case {
 
 	/**
+	 * Test axisbuilder_clean() - note this is a basic type test as WP core already
+	 * has coverage for sanitized_text_field()
+	 *
+	 * @since 1.0
+	 */
+	public function test_axisbuilder_clean() {
+
+		$this->assertInternalType( 'string', axisbuilder_clean( 'cleaned' ) );
+	}
+
+	/**
 	 * Test test_axisbuilder_get_core_supported_themes()
 	 *
 	 * @since 1.0

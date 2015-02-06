32c1d58a6db0b96c0e49535971c06bacba31926c
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-02-06 20:57:06 +0545

    Delete posts + data functionality upon uninstall

--- uninstall.php
+++ uninstall.php
@@ -14,9 +14,17 @@ if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 global $wpdb;
 
+$status_options = apply_filters( 'axisbuilder_status_options', array( 'uninstall_data' => false ) );
+
 // Roles + caps
 include_once( 'includes/class-builder-install.php' );
 AB_Install::remove_roles();
 
 // Delete options
 $wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'axisbuilder_%';" );
+
+if ( ! empty( $status_options['uninstall_data'] ) ) {
+	// Delete posts + data
+	$wpdb->query( "DELETE FROM {$wpdb->posts} WHERE post_type IN ( 'portfolio' );" );
+	$wpdb->query( "DELETE FROM {$wpdb->postmeta} meta LEFT JOIN {$wpdb->posts} posts ON posts.ID = meta.post_id WHERE wp.ID IS NULL;" );
+}

06b5247ce330a941ba8aa9b2ef6906aeec7c9b1f
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-02-07 00:42:10 +0545

    Add style attribute for layout meta-box

--- includes/admin/meta-boxes/class-builder-meta-box-layout-data.php
+++ includes/admin/meta-boxes/class-builder-meta-box-layout-data.php
@@ -33,7 +33,7 @@ class AB_Meta_Box_Layout_Data {
 				do_action( 'axisbuilder_layout_data_start', $post->ID );
 
 				// Layout
-				axisbuilder_wp_select( array( 'id' => 'layout', 'label' => __( 'Layout', 'axisbuilder' ), 'options' => array(
+				axisbuilder_wp_select( array( 'id' => 'layout', 'style' => 'width: 100%', 'label' => __( 'Layout', 'axisbuilder' ), 'options' => array(
 					'default'       => __( 'Default Layout', 'axisbuilder' ),
 					'fullsize'      => __( 'No Sidebar', 'axisbuilder' ),
 					'sidebar_left'  => __( 'Left Sidebar', 'axisbuilder' ),
@@ -41,10 +41,10 @@ class AB_Meta_Box_Layout_Data {
 				), 'desc_tip' => true, 'description' => __( 'Select the desired Page layout.', 'axisbuilder' ) ) );
 
 				// Sidebar
-				axisbuilder_wp_select( array( 'id' => 'sidebar', 'label' => __( 'Sidebar Settings', 'axisbuilder' ), 'desc_tip' => true, 'description' => __( 'Choose a custom sidebar for this entry.', 'axisbuilder' ), 'options' => axisbuilder_get_registered_sidebars() ) );
+				axisbuilder_wp_select( array( 'id' => 'sidebar', 'style' => 'width: 100%', 'label' => __( 'Sidebar Settings', 'axisbuilder' ), 'desc_tip' => true, 'description' => __( 'Choose a custom sidebar for this entry.', 'axisbuilder' ), 'options' => axisbuilder_get_registered_sidebars() ) );
 
 				// Title Bar
-				axisbuilder_wp_select( array( 'id' => 'header_title_bar', 'label' => __( 'Title Bar Settings', 'axisbuilder' ), 'options' => array(
+				axisbuilder_wp_select( array( 'id' => 'header_title_bar', 'style' => 'width: 100%', 'label' => __( 'Title Bar Settings', 'axisbuilder' ), 'options' => array(
 					'default'              => __( 'Default Layout', 'axisbuilder' ),
 					'title_bar_breadcrumb' => __( 'Display title and breadcrumbs', 'axisbuilder' ),
 					'title_bar'            => __( 'Display only title', 'axisbuilder' ),
@@ -52,14 +52,14 @@ class AB_Meta_Box_Layout_Data {
 				), 'desc_tip' => true, 'description' => __( 'Display the Title Bar with Page Title and Breadcrumb Navigation?', 'axisbuilder' ) ) );
 
 				// Header Transparency
-				axisbuilder_wp_select( array( 'id' => 'header_transparency', 'label' => __( 'Activate Header transparency', 'axisbuilder' ), 'options' => array(
+				axisbuilder_wp_select( array( 'id' => 'header_transparency', 'style' => 'width: 100%', 'label' => __( 'Activate Header transparency', 'axisbuilder' ), 'options' => array(
 					'default'                          => __( 'No transparency', 'axisbuilder' ),
 					'header_transparent'               => __( 'Transparent Header', 'axisbuilder' ),
 					'header_transparent header_glassy' => __( 'Transparent & Glassy Header', 'axisbuilder' )
 				), 'desc_tip' => true, 'description' => __( 'If checked the header will be transparent and once the user scrolls down it will fade in.', 'axisbuilder' ) ) );
 
 				// Footer Settings
-				axisbuilder_wp_select( array( 'id' => 'footer', 'label' => __( 'Footer Settings', 'axisbuilder' ), 'options' => array(
+				axisbuilder_wp_select( array( 'id' => 'footer', 'style' => 'width: 100%', 'label' => __( 'Footer Settings', 'axisbuilder' ), 'options' => array(
 					'footer_both' => __( 'Both Widgets and Socket', 'axisbuilder' ),
 					'widget_only' => __( 'Only Widgets (No Socket)', 'axisbuilder' ),
 					'socket_only' => __( 'Only Socket (No Widgets)', 'axisbuilder' ),

37858fa1f4c2a8f5d79f38dc00fde235f9afd08d
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-02-07 19:39:24 +0545

    AJAX Page search boxes script

--- assets/scripts/admin/enhanced-select.js
+++ assets/scripts/admin/enhanced-select.js
@@ -56,7 +56,7 @@ jQuery( function( $ ) {
 		.on( 'axisbuilder-enhanced-select-init', function() {
 
 			// Regular select boxes
-			$( ':input.axisbuilder-enhanced-select, :input.chosen_select' ).filter( ':not(.enhanced)' ).each( function() {
+			$( ':input.axisbuilder-enhanced-select' ).filter( ':not(.enhanced)' ).each( function() {
 				var select2_args = $.extend({
 					minimumResultsForSearch: 10,
 					allowClear:  $( this ).data( 'allow_clear' ) ? true : false,
@@ -66,12 +66,72 @@ jQuery( function( $ ) {
 				$( this ).select2( select2_args ).addClass( 'enhanced' );
 			});
 
-			$( ':input.axisbuilder-enhanced-select-nostd, :input.chosen_select_nostd' ).filter( ':not(.enhanced)' ).each( function() {
+			$( ':input.axisbuilder-enhanced-select-nostd' ).filter( ':not(.enhanced)' ).each( function() {
 				var select2_args = $.extend({
 					minimumResultsForSearch: 10,
 					allowClear:  true,
 					placeholder: $( this ).data( 'placeholder' )
 				}, getEnhancedSelectFormatString() );
+
+				$( this ).select2( select2_args ).addClass( 'enhanced' );
+			});
+
+			// Ajax page search boxes
+			$( ':input.axisbuilder-page-search' ).filter( ':not(.enhanced)' ).each( function() {
+				var select2_args = {
+					allowClear:  $( this ).data( 'allow_clear' ) ? true : false,
+					placeholder: $( this ).data( 'placeholder' ),
+					minimumInputLength: $( this ).data( 'minimum_input_length' ) ? $( this ).data( 'minimum_input_length' ) : '3',
+					escapeMarkup: function( m ) {
+						return m;
+					},
+					ajax: {
+						url:         axisbuilder_enhanced_select_params.ajax_url,
+						dataType:    'json',
+						quietMillis: 250,
+						data: function( term, page ) {
+							return {
+								term:     term,
+								action:   'axisbuilder_json_search_pages',
+								security: axisbuilder_enhanced_select_params.search_pages_nonce
+							};
+						},
+						results: function( data, page ) {
+							var terms = [];
+							if ( data ) {
+								$.each( data, function( id, text ) {
+									terms.push( { id: id, text: text } );
+								});
+							}
+							return { results: terms };
+						},
+						cache: true
+					}
+				};
+				if ( $( this ).data( 'multiple' ) === true ) {
+					select2_args.multiple = true;
+					select2_args.initSelection = function( element, callback ) {
+						var data     = $.parseJSON( element.attr( 'data-selected' ) );
+						var selected = [];
+
+						$( element.val().split( "," ) ).each( function( i, val ) {
+							selected.push( { id: val, text: data[ val ] } );
+						});
+						return callback( selected );
+					};
+					select2_args.formatSelection = function( data ) {
+						return '<div class="selected-option" data-id="' + data.id + '">' + data.text + '</div>';
+					};
+				} else {
+					select2_args.multiple = false;
+					select2_args.initSelection = function( element, callback ) {
+						var data = {id: element.val(), text: element.attr( 'data-selected' )};
+						return callback( data );
+					};
+				}
+
+
+				select2_args = $.extend( select2_args, getEnhancedSelectFormatString() );
 
 				$( this ).select2( select2_args ).addClass( 'enhanced' );
 			});

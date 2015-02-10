94bd0bf6661548e0ca5b1fa636d5ee45cf58adf2
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-02-10 08:11:35 +0545

    Footer Socket and Widgets area for Layout Meta-Box

--- includes/admin/meta-boxes/class-builder-meta-box-layout-data.php
+++ includes/admin/meta-boxes/class-builder-meta-box-layout-data.php
@@ -43,6 +43,15 @@ class AB_Meta_Box_Layout_Data {
 				// Sidebar
 				axisbuilder_wp_select( array( 'id' => 'sidebar', 'class' => 'select side', 'label' => __( 'Sidebar Settings', 'axisbuilder' ), 'desc_side' => true, 'desc_tip' => false, 'desc_class' => 'side', 'description' => __( 'Choose a custom sidebar for this entry.', 'axisbuilder' ), 'options' => axisbuilder_get_registered_sidebars( array( 'default' => 'Default Sidebar' ), array( 'Display Everywhere' ) ) ) );
 
+				// Footer Settings
+				axisbuilder_wp_select( array( 'id' => 'footer', 'class' => 'select side', 'label' => __( 'Footer Settings', 'axisbuilder' ), 'options' => array(
+					'default'     => __( 'Default Socket and Widgets', 'axisbuilder' ),
+					'footer_both' => __( 'Both Socket and Widgets', 'axisbuilder' ),
+					'widget_only' => __( 'Only Widgets (No Socket)', 'axisbuilder' ),
+					'socket_only' => __( 'Only Socket (No Widgets)', 'axisbuilder' ),
+					'footer_hide' => __( 'Hide Socket and Widgets', 'axisbuilder' )
+				), 'desc_side' => true, 'desc_tip' => false, 'desc_class' => 'side', 'description' => __( 'Display the socket and footer widgets?', 'axisbuilder' ) ) );
+
 				// Title Bar
 				axisbuilder_wp_select( array( 'id' => 'header_title_bar', 'class' => 'select side', 'label' => __( 'Title Bar Settings', 'axisbuilder' ), 'options' => array(
 					'default'              => __( 'Default Layout', 'axisbuilder' ),
@@ -50,14 +59,6 @@ class AB_Meta_Box_Layout_Data {
 					'title_bar'            => __( 'Display only title', 'axisbuilder' ),
 					'hidden_title_bar'     => __( 'Hide both', 'axisbuilder' )
 				), 'desc_side' => true, 'desc_tip' => false, 'desc_class' => 'side', 'description' => __( 'Display the Title Bar with Page Title and Breadcrumb Navigation?', 'axisbuilder' ) ) );
-
-				// Footer Settings
-				axisbuilder_wp_select( array( 'id' => 'footer', 'class' => 'select side', 'label' => __( 'Footer Settings', 'axisbuilder' ), 'options' => array(
-					'footer_both' => __( 'Both Widgets and Socket', 'axisbuilder' ),
-					'widget_only' => __( 'Only Widgets (No Socket)', 'axisbuilder' ),
-					'socket_only' => __( 'Only Socket (No Widgets)', 'axisbuilder' ),
-					'footer_none' => __( 'Don\'t Display Both', 'axisbuilder' )
-				), 'desc_side' => true, 'desc_tip' => false, 'desc_class' => 'side', 'description' => __( 'Display the footer widgets?', 'axisbuilder' ) ) );
 
 				// Header Transparency
 				axisbuilder_wp_select( array( 'id' => 'header_transparency', 'class' => 'select side', 'label' => __( 'Header visibility and transparency', 'axisbuilder' ), 'options' => array(

739dfeabf44c2cc0b67cbeffec5033aacda07104
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-02-12 21:45:59 +0545

    Only run the uninstaller if the "Uninstall on Delete" option is checked

--- uninstall.php
+++ uninstall.php
@@ -14,18 +14,19 @@ if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-global $wpdb;
-
-$status_options = apply_filters( 'axisbuilder_status_options', array( 'uninstall_data' => false ) );
-
-// Roles + caps
-include_once( 'includes/class-builder-install.php' );
-AB_Install::remove_roles();
-
-// Delete options
-$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'axisbuilder_%';" );
+$status_options = apply_filters( 'axisbuilder_status_options', array() );
 
 if ( ! empty( $status_options['uninstall_data'] ) ) {
+
+	global $wpdb;
+
+	// Roles + caps
+	include_once( 'includes/class-builder-install.php' );
+	AB_Install::remove_roles();
+
+	// Delete options
+	$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'axisbuilder_%';" );
+
 	// Delete posts + data
 	$wpdb->query( "DELETE FROM {$wpdb->posts} WHERE post_type IN ( 'portfolio' );" );
 	$wpdb->query( "DELETE FROM {$wpdb->postmeta} meta LEFT JOIN {$wpdb->posts} posts ON posts.ID = meta.post_id WHERE wp.ID IS NULL;" );

167bebe442115862539c1a7a3bda3c4c05c8747a
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-02-18 04:27:10 +0545

    Welcom Page Credits and Translators

--- includes/admin/class-builder-admin-welcome.php
+++ includes/admin/class-builder-admin-welcome.php
@@ -132,6 +132,187 @@ class AB_Admin_Welcome {
 	}
 
 	/**
+	 * Intro text/links shown on all about pages.
+	 */
+	private function intro() {
+
+		// Flush after upgrades
+		if ( ! empty( $_GET['ab-updated'] ) || ! empty( $_GET['ab-installed'] ) ) {
+			flush_rewrite_rules();
+		}
+
+		// Drop minor version if 0
+		$major_version = substr( AB()->version, 0, 3 );
+		?>
+		<h1><?php printf( __( 'Welcome to AxisBuilder %s', 'axisbuilder' ), $major_version ); ?></h1>
+
+		<div class="about-text axisbuilder-about-text">
+			<?php
+				if ( ! empty( $_GET['ab-installed'] ) ) {
+					$message = __( 'Thanks, all done!', 'axisbuilder' );
+				} elseif ( ! empty( $_GET['ab-updated'] ) ) {
+					$message = __( 'Thank you for updating to the latest version!', 'axisbuilder' );
+				} else {
+					$message = __( 'Thanks for installing!', 'axisbuilder' );
+				}
+
+				printf( __( '%s AxisBuilder %s is more powerful, stable and secure than ever before. We hope you enjoy using it.', 'axisbuilder' ), $message, $major_version );
+			?>
+		</div>
+
+		<div class="axisbuilder-badge"><?php printf( __( 'Version %s', 'axisbuilder' ), AB()->version ); ?></div>
+
+		<p class="axisbuilder-actions">
+			<a href="<?php echo admin_url('admin.php?page=axisbuilder-settings'); ?>" class="button button-primary"><?php _e( 'Settings', 'axisbuilder' ); ?></a>
+			<a href="<?php echo esc_url( apply_filters( 'axisbuilder_docs_url', 'http://docs.axisthemes.com/documentation/plugins/axisbuilder/', 'axisbuilder' ) ); ?>" class="button button-secondary"><?php _e( 'Documentation', 'axisbuilder' ); ?></a>
+			<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://axisthemes.com/axisbuilder/" data-text="A open-source (free) #builder plugin for #WordPress that helps you build unique and modern page layouts smartly. Beautifully." data-via="AxisThemes" data-size="large" data-hashtags="AxisThemes">Tweet</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</p>
+
+		<h2 class="nav-tab-wrapper">
+			<a class="nav-tab <?php if ( $_GET['page'] == 'axisbuilder-about' ) echo 'nav-tab-active'; ?>" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'axisbuilder-about' ), 'index.php' ) ) ); ?>">
+				<?php _e( "What's New", 'axisbuilder' ); ?>
+			</a><a class="nav-tab <?php if ( $_GET['page'] == 'axisbuilder-credits' ) echo 'nav-tab-active'; ?>" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'axisbuilder-credits' ), 'index.php' ) ) ); ?>">
+				<?php _e( 'Credits', 'axisbuilder' ); ?>
+			</a><a class="nav-tab <?php if ( $_GET['page'] == 'axisbuilder-translators' ) echo 'nav-tab-active'; ?>" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'axisbuilder-translators' ), 'index.php' ) ) ); ?>">
+				<?php _e( 'Translators', 'axisbuilder' ); ?>
+			</a>
+		</h2>
+		<?php
+	}
+
+	/**
+	 * Output the about screen.
+	 */
+	public function about_screen() {
+		?>
+		<div class="wrap about-wrap">
+
+			<?php $this->intro(); ?>
+
+			<!--<div class="changelog point-releases"></div>-->
+
+		</div>
+		<?php
+	}
+
+	/**
+	 * Output the credits screen.
+	 */
+	public function credits_screen() {
+		?>
+		<div class="wrap about-wrap">
+
+			<?php $this->intro(); ?>
+
+			<p class="about-description"><?php printf( __( 'AxisBuilder is developed and maintained by a worldwide team of passionate individuals and backed by an awesome developer community. Want to see your name? <a href="%s">Contribute to AxisBuilder</a>.', 'axisbuilder' ), 'https://github.com/axisthemes/axisbuilder/blob/master/CONTRIBUTING.md' ); ?></p>
+
+			<?php echo $this->contributors(); ?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Output the translators screen.
+	 */
+	public function translators_screen() {
+		?>
+		<div class="wrap about-wrap">
+
+			<?php $this->intro(); ?>
+
+			<p class="about-description"><?php printf( __( 'AxisBuilder has been kindly translated into several other languages thanks to our translation team. Want to see your name? <a href="%s">Translate AxisBuilder</a>.', 'axisbuilder' ), 'https://www.transifex.com/projects/p/axisbuilder/' ); ?></p>
+
+			<?php
+				// Have to use this to get the list until the API is open...
+				/*
+				$contributor_json = json_decode( 'string from https://www.transifex.com/api/2/project/axisbuilder/languages/', true );
+
+				$contributors = array();
+
+				foreach ( $contributor_json as $group ) {
+					$contributors = array_merge( $contributors, $group['coordinators'], $group['reviewers'], $group['translators'] );
+				}
+
+				$contributors = array_filter( array_unique( $contributors ) );
+
+				natsort( $contributors );
+
+				foreach ( $contributors as $contributor ) {
+					echo htmlspecialchars( '<a href="https://www.transifex.com/accounts/profile/' . $contributor . '">' . $contributor . '</a>, ' );
+				}
+				*/
+			?>
+
+			<p class="wp-credits-list">
+				<a href="https://www.transifex.com/accounts/profile/axisthemes">AxisThemes</a>
+			</p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Render Contributors List.
+	 *
+	 * @return string $contributor_list HTML formatted list of contributors.
+	 */
+	public function contributors() {
+		$contributors = $this->get_contributors();
+
+		if ( empty( $contributors ) ) {
+			return '';
+		}
+
+		$contributor_list = '<ul class="wp-people-group">';
+
+		foreach ( $contributors as $contributor ) {
+			$contributor_list .= '<li class="wp-person">';
+			$contributor_list .= sprintf( '<a href="%s" title="%s">',
+				esc_url( 'https://github.com/' . $contributor->login ),
+				esc_html( sprintf( __( 'View %s', 'axisbuilder' ), $contributor->login ) )
+			);
+			$contributor_list .= sprintf( '<img src="%s" width="64" height="64" class="gravatar" alt="%s" />', esc_url( $contributor->avatar_url ), esc_html( $contributor->login ) );
+			$contributor_list .= '</a>';
+			$contributor_list .= sprintf( '<a class="web" href="%s">%s</a>', esc_url( 'https://github.com/' . $contributor->login ), esc_html( $contributor->login ) );
+			$contributor_list .= '</a>';
+			$contributor_list .= '</li>';
+		}
+
+		$contributor_list .= '</ul>';
+
+		return $contributor_list;
+	}
+
+	/**
+	 * Retrieve list of contributors from GitHub.
+	 *
+	 * @return mixed
+	 */
+	public function get_contributors() {
+		$contributors = get_transient( 'axisbuilder_contributors' );
+
+		if ( false !== $contributors ) {
+			return $contributors;
+		}
+
+		$response = wp_remote_get( 'https://api.github.com/repos/axisthemes/axisbuilder/contributors', array( 'sslverify' => false ) );
+
+		if ( is_wp_error( $response ) || 200 != wp_remote_retrieve_response_code( $response ) ) {
+			return array();
+		}
+
+		$contributors = json_decode( wp_remote_retrieve_body( $response ) );
+
+		if ( ! is_array( $contributors ) ) {
+			return array();
+		}
+
+		set_transient( 'axisbuilder_contributors', $contributors, HOUR_IN_SECONDS );
+
+		return $contributors;
+	}
+
+	/**
 	 * Sends user to the welcome page on first activation.
 	 */
 	public function welcome() {

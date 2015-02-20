2f71241a7648592c9a65e4a53bf09b7076e7e3b5
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-02-20 21:32:14 +0545

    Display for each iconfonts

--- includes/admin/class-builder-admin-iconfonts.php
+++ includes/admin/class-builder-admin-iconfonts.php
@@ -31,16 +31,15 @@ class AB_Admin_Iconfonts {
 	 * Returns the iconfonts to show in admin.
 	 */
 	public static function get_iconfonts() {
-		$iconfonts = get_option( 'axisbuilder_custom_iconfonts' );
-		$counter   = count( $iconfonts );
+		$fonts = get_option( 'axisbuilder_custom_iconfonts' );
+		$count = count( $fonts );
 
-		foreach ( $iconfonts as $iconfont => $info ) {
+		foreach ( $fonts as $iconfont => $info ) {
 			$chars   = array();
 			$charmap = array();
 
-			$output   = '<div id="dashboard-widgets" class="metabox-holder">';
-			$output  .= '<div id="postbox-container-' . count( $iconfont ) . '" class="postbox-container">';
-			$output  .= '<div class="postbox">';
+			$output  = '<div class="iconfonts-container-' . count( $iconfont ) . ' metabox-holder">';
+			$output .= '<div class="postbox">';
 
 			// Include Charmap config file
 			include( AB_UPLOAD_DIR . $info['include'] . '/' . $info['config'] );
@@ -51,19 +50,26 @@ class AB_Admin_Iconfonts {
 
 			if ( ! empty( $charmap ) ) {
 				foreach ( $charmap as $chars ) {
-					$count = count( $chars );
+					$iconfont_count = count( $chars );
 				}
 
 				$title  = ( $iconfont === 'icomoon' || $iconfont === 'Fontawesome' ) ? __( 'Default Icons', 'axisbuilder' ) : ucfirst( $iconfont );
-				$delete = ( $counter !== 1 ) ? '<button class="button button-secondary button-small del-iconfont" data-delete="' . $iconfont . '" data-title="' . __( 'Delete This Icon Set', 'axisbuilder' ) . '">' . __( 'Delete Icon Set', 'axisbuilder' ) . '</button></h3>' : '</h3>';
+				$delete = ( $count !== 1 ) ? '<button class="button button-secondary button-small del-iconfont" data-delete="' . $iconfont . '" data-title="' . __( 'Delete This Icon Set', 'axisbuilder' ) . '">' . __( 'Delete Icon Set', 'axisbuilder' ) . '</button></h3>' : '</h3>';
 
-				$output .= '<h3 class="iconfont-title"><strong>' . $title . '</strong><span class="fonts-count count-' . $iconfont . '">' . $count . '</span>' . $delete;
+				$output .= '<h3 class="iconfont-title"><strong>' . $title . '</strong><span class="iconfont-count count-' . $iconfont_count . '">' . number_format_i18n( $iconfont_count ) . '</span>' . $delete;
+				$output .= '<div class="inside"><div class="iconfont-action"></div>';
+				$output .= '<div class="iconfont-search">';
+				$output .= '<ul class="icons-list icon">';
 
+				foreach ( $charmap as $chars ) {
+					foreach ( $chars as $char ) {
+						$output .= '';
+					}
+				}
+
+				$output .= '</ul></div></div></div></div>';
+				echo $output;
 			}
-
-			$output .= '</div></div></div>';
-
-			echo $output;
 		}
 	}
 }

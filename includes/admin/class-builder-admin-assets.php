56410f7c8dac31b45050697eeed57c65957128a5
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-05 19:12:14 +0545

    Properly load iconfonts page necessary scripts :P

--- includes/admin/class-builder-admin-assets.php
+++ includes/admin/class-builder-admin-assets.php
@@ -60,7 +60,7 @@ class AB_Admin_Assets {
 			wp_enqueue_style( 'axisbuilder-admin-sidebars', AB()->plugin_url() . '/assets/styles/sidebars.css', array(), AB_VERSION );
 		}
 
-		if ( in_array( $screen->id, array( 'axis-builder_page_axisbuilder-iconfonts' ) ) ) {
+		if ( in_array( $screen->id, array( 'axisbuilder_page_axisbuilder-iconfonts' ) ) ) {
 			wp_enqueue_style( 'axisbuilder-admin-iconfonts', AB()->plugin_url() . '/assets/styles/iconfonts.css', array(), AB_VERSION );
 		}
 	}
@@ -213,8 +213,8 @@ class AB_Admin_Assets {
 			wp_localize_script( 'axisbuilder-admin-sidebars', 'axisbuilder_admin_sidebars', $params );
 		}
 
-		// Icon-Fonts Specific
-		if ( in_array( $screen->id, array( 'axis-builder_page_axisbuilder-iconfonts' ) ) ) {
+		// Iconfonts Specific
+		if ( in_array( $screen->id, array( 'axisbuilder_page_axisbuilder-iconfonts' ) ) ) {
 			wp_enqueue_media();
 			wp_enqueue_script( 'media-upload' );
 			wp_enqueue_script( 'axisbuilder-admin-iconfonts', AB()->plugin_url() . '/assets/scripts/admin/iconfonts' . $suffix . '.js', array( 'jquery', 'underscore', 'backbone' ), AB_VERSION );

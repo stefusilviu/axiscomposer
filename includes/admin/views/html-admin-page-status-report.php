b4bf4f2cf1547acced0b140099c660258ae8848d
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-08 15:57:07 +0545

    Fix WP Remote Post check error message

--- includes/admin/views/html-admin-page-status-report.php
+++ includes/admin/views/html-admin-page-status-report.php
@@ -163,9 +163,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 			} else {
 				$posting['wp_remote_post']['note']    = __( 'wp_remote_post() failed. Contact your hosting provider.', 'axisbuilder' );
 				if ( is_wp_error( $response ) ) {
-					$posting['wp_remote_post']['note'] .= ' ' . sprintf( __( 'Error: %s', 'axisbuilder' ), wc_clean( $response->get_error_message() ) );
+					$posting['wp_remote_post']['note'] .= ' ' . sprintf( __( 'Error: %s', 'axisbuilder' ), axisbuilder_clean( $response->get_error_message() ) );
 				} else {
-					$posting['wp_remote_post']['note'] .= ' ' . sprintf( __( 'Status code: %s', 'axisbuilder' ), wc_clean( $response['response']['code'] ) );
+					$posting['wp_remote_post']['note'] .= ' ' . sprintf( __( 'Status code: %s', 'axisbuilder' ), axisbuilder_clean( $response['response']['code'] ) );
 				}
 				$posting['wp_remote_post']['success'] = false;
 			}

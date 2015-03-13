530e930ea57c1fec30c17fa9da731fe305513286
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-13 09:03:01 +0545

    Fix some typo and space

--- includes/admin/meta-boxes/class-builder-meta-box-builder-data.php
+++ includes/admin/meta-boxes/class-builder-meta-box-builder-data.php
@@ -2,7 +2,7 @@
 /**
  * Page Builder Data
  *
- * Displays the page builder data box, tabbed, with several drag and drop canvas elements.
+ * Displays the page builder data meta box, tabbed, with several drag and drop canvas elements.
  *
  * @class       AB_Meta_Box_Builder_Data
  * @package     AxisBuilder/Admin/Meta Boxes
@@ -29,7 +29,7 @@ class AB_Meta_Box_Builder_Data {
 		wp_nonce_field( 'axisbuilder_save_data', 'axisbuilder_meta_nonce' );
 
 		?>
-		<input type="hidden" name="axisbuilder_status" value="<?php echo esc_attr( is_pagebuilder_active( $post->ID ) ); ?>"/>
+		<input type="hidden" name="axisbuilder_status" value="<?php echo esc_attr( is_pagebuilder_active( $post->ID ) ); ?>" />
 		<div id="axis-pagebuilder" class="axisbuilder-shortcodes axisbuilder-style">
 			<div id="axisbuilder-panels" class="panel-wrap">
 				<ul class="axisbuilder-tabs">

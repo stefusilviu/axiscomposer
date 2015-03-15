fc6fa8ed3e4dee7b9e386f214b17bb9b8916dc59
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-15 22:21:47 +0545

    Datepicker trigger on each meta boxes

--- assets/scripts/admin/meta-boxes.js
+++ assets/scripts/admin/meta-boxes.js
@@ -15,4 +15,14 @@ jQuery( function ( $ ) {
 	});
 	$( 'ul.axisbuilder-tabs li' ).eq( 0 ).find( 'a' ).click();
 
+	// DATE PICKER
+	$( 'body' ).on( 'axisbuilder-init-datepickers', function() {
+		$( '.date-picker-field, .date-picker' ).datepicker({
+			dateFormat: 'yy-mm-dd',
+			numberOfMonths: 1,
+			showButtonPanel: true,
+		});
+	});
+	$( 'body' ).trigger( 'axisbuilder-init-datepickers' );
+
 });

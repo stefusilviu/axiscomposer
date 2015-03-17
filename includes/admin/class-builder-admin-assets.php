b7fcc3ff44474e665573265339a6c777bc4927bd
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-17 23:10:05 +0545

    Type add

--- includes/admin/class-builder-admin-assets.php
+++ includes/admin/class-builder-admin-assets.php
@@ -161,7 +161,7 @@ class AB_Admin_Assets {
 			wp_localize_script( 'axisbuilder-admin-builder-meta-boxes', 'axisbuilder_admin_meta_boxes_builder', $params );
 		}
 
-		// Layouts
+		// Layouts Settings
 		if ( in_array( $screen->id, axisbuilder_get_layout_supported_screens() ) ) {
 			wp_enqueue_script( 'axisbuilder-admin-layout-meta-boxes', AB()->plugin_url() . '/assets/scripts/admin/meta-boxes-layout' . $suffix . '.js', array( 'axisbuilder-admin-meta-boxes' ), AB_VERSION );
 		}

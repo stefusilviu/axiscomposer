a50af24df53977d770143ba6859c3908e676226e
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-18 02:24:06 +0545

    Tweaks for page-builder metabox at first position xD

--- assets/scripts/admin/meta-boxes-position.js
+++ assets/scripts/admin/meta-boxes-position.js
@@ -3,12 +3,12 @@ jQuery( window ).load( function() {
 
 	// Page Builder at the first position
 	jQuery( function() {
-		var metabox = jQuery( '#normal-sortables' ),
-			postbox = metabox.find( '.postbox' ),
-			parents = jQuery( '#axisbuilder-canvas' ).find( '.canvas-area' ).parents( '.postbox:eq(0)' );
+		var builder = jQuery( '#axisbuilder-editor' ),
+			metabox = jQuery( '#normal-sortables' ),
+			postbox = metabox.find( '.postbox' );
 
-		if ( parents.length && ( postbox.index( parents ) !== 0 ) ) {
-			parents.prependTo( metabox );
+		if ( builder.length && ( postbox.index( builder ) !== 0 ) ) {
+			builder.prependTo( metabox );
 
 			// Re-save the postbox Order
 			window.postboxes.save_order( pagenow );

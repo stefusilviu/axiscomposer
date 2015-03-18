c26dc9274033797bfa953d9d80cab16f1c46542f
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-18 16:40:59 +0545

    Tweaks for axisbuilder_log

--- assets/scripts/admin/meta-boxes-logger.js
+++ assets/scripts/admin/meta-boxes-logger.js
@@ -1,22 +1,14 @@
 /* global axisbuilder_admin_meta_boxes_builder, console */
 function axisbuilder_log( text, type ) {
-	if ( typeof console === 'undefined' ) {
+	if ( typeof console === undefined ) {
 		return true;
 	}
 
-	if ( typeof type === 'undefined' ) {
-		type = 'logger';
-	}
-
-	if ( type === false ) {
-		console.log( text );
-	} else {
-		type = 'AB_' + type.toUpperCase();
-		console.log( '[' + type + '] - ' + text );
-	}
+	var logger = ( typeof type !== undefined ) ? ( ( type === true ) ? text : ( '[AB_' + type + '] - ' + text ) ) : ( '[AB_Logger] - ' + text );
+	console.log( type ? logger : text );
 }
 
 // Logger
 if ( axisbuilder_admin_meta_boxes_builder.debug_mode === 'yes' ) {
-	new axisbuilder_log( 'Page Builder Debug Mode is enabled', 'Debug' );
+	new axisbuilder_log( 'Page Builder Debug Mode is enabled', false );
 }

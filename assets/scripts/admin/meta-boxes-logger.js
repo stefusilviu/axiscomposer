ee59328f0bd65d7bd80b7abef6e8fed7b8ae4575
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-18 22:04:29 +0545

    Capitalize 'type' first char and lowercase other for logger

--- assets/scripts/admin/meta-boxes-logger.js
+++ assets/scripts/admin/meta-boxes-logger.js
@@ -1,14 +1,14 @@
 /* global axisbuilder_admin_meta_boxes_builder, console */
-function axisbuilder_log( text, type ) {
+function axisbuilder_log( string, type ) {
 	if ( typeof console === undefined ) {
 		return true;
 	}
 
-	var logger = ( typeof type !== undefined ) ? ( ( type === true ) ? text : ( '[AB_' + type + '] - ' + text ) ) : ( '[AB_Logger] - ' + text );
-	console.log( type ? logger : text );
+	var logger = ( typeof type !== undefined ) ? ( ( type === true ) ? string : ( '[AB_' + type.charAt(0).toUpperCase() + type.slice(1).toLowerCase() + '] - ' + string ) ) : ( '[AB_Logger] - ' + string );
+	console.log( type ? logger : string );
 }
 
 // Logger
 if ( axisbuilder_admin_meta_boxes_builder.debug_mode === 'yes' ) {
-	new axisbuilder_log( 'Page Builder Debug Mode is enabled', false );
+	new axisbuilder_log( 'Page Builder Debug Mode is enabled', 'debug' );
 }

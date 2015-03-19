3dfc178ab4e778e147b356ae245ac7e87fff94d1
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-19 13:56:43 +0545

    Select availability + Hidden options

--- assets/scripts/admin/admin.js
+++ assets/scripts/admin/admin.js
@@ -11,7 +11,47 @@ jQuery( function ( $ ) {
 
 	// Add tiptip to parent element for widefat tables
 	$( '.parent-tips' ).each( function() {
-		$( this ).closest( 'a, th' ).attr( 'data-tip', $( this ).data( 'tip' ) ).tipTip( tiptip_args ).css( 'cursor', 'help' );
+		$(this).closest( 'a, th' ).attr( 'data-tip', $( this ).data( 'tip' ) ).tipTip( tiptip_args ).css( 'cursor', 'help' );
+	});
+
+	// Select availability
+	$( 'select.availability' ).change( function() {
+
+		if ( $(this).val() === 'all' ) {
+			$(this).closest( 'tr' ).next( 'tr' ).hide();
+		} else {
+			$(this).closest( 'tr' ).next( 'tr' ).show();
+		}
+
+	}).change();
+
+	// Hidden options
+	$( '.hide_options_if_checked' ).each( function() {
+
+		$(this).find( 'input:eq(0)' ).change( function() {
+
+			if ( $(this).is( ':checked' ) ) {
+				$(this).closest( 'fieldset, tr' ).nextUntil( '.hide_options_if_checked, .show_options_if_checked', '.hidden_option' ).hide();
+			} else {
+				$(this).closest( 'fieldset, tr' ).nextUntil( '.hide_options_if_checked, .show_options_if_checked', '.hidden_option' ).show();
+			}
+
+		}).change();
+
+	});
+
+	$( '.show_options_if_checked' ).each( function() {
+
+		$(this).find( 'input:eq(0)' ).change( function() {
+
+			if ( $(this).is( ':checked' ) ) {
+				$(this).closest( 'fieldset, tr' ).nextUntil( '.hide_options_if_checked, .show_options_if_checked', '.hidden_option' ).show();
+			} else {
+				$(this).closest( 'fieldset, tr' ).nextUntil( '.hide_options_if_checked, .show_options_if_checked', '.hidden_option' ).hide();
+			}
+
+		}).change();
+
 	});
 
 });

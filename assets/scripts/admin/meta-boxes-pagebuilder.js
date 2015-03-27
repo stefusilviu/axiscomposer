5a4efd0d519b516ae6bf9ef7ad951e7633f0a609
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-27 15:38:39 +0545

    Added init_tooltip

--- assets/scripts/admin/meta-boxes-pagebuilder.js
+++ assets/scripts/admin/meta-boxes-pagebuilder.js
@@ -114,6 +114,17 @@ jQuery( function( $ ) {
 			$( '#axisbuilder-editor' ).unblock();
 		},
 
+		init_tiptip: function() {
+			$( '#tiptip_holder' ).removeAttr( 'style' );
+			$( '#tiptip_arrow' ).removeAttr( 'style' );
+			$( '.tips' ).tipTip({
+				'attribute': 'data-tip',
+				'fadeIn': 50,
+				'fadeOut': 50,
+				'delay': 200
+			});
+		},
+
 		history_snapshot: function( timeout ) {
 			setTimeout( function() {
 				$( '.canvas-area' ).trigger( 'axisbuilder-storage-update' );

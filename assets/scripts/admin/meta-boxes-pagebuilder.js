d5a5079f774e27e6d9b3f02749305ee16814f06f
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-03-30 00:16:27 +0545

    Better way to use one data

--- assets/scripts/admin/meta-boxes-pagebuilder.js
+++ assets/scripts/admin/meta-boxes-pagebuilder.js
@@ -678,9 +678,8 @@ jQuery( function( $ ) {
 				var size_count     = 0,
 					content_val    = '',
 					column_size    = axisbuilder_meta_boxes_builder_data.col_size,
-					column_sizes   = { 'ab_one_full': 1.00, 'ab_four_fifth': 0.80, 'ab_three_fourth': 0.75, 'ab_two_third': 0.66, 'ab_three_fifth': 0.60, 'ab_one_half': 0.50, 'ab_two_fifth': 0.40, 'ab_one_third': 0.33, 'ab_one_fourth': 0.25, 'ab_one_fifth': 0.20 },
 					content_fields = scope.find( '>textarea[data-name="text-shortcode"]' ),
-					current_field, current_content, current_parents, current_size;
+					current_field, current_content, current_parents, current_size, next_size;
 
 				for ( var i = 0; i < content_fields.length; i++ ) {
 					current_field   = $( content_fields[i] );
@@ -690,7 +689,13 @@ jQuery( function( $ ) {
 					// Check for column to add/remove first class
 					if ( current_parents.length ) {
 						current_size = current_parents.data( 'width' );
-						size_count  += column_sizes[current_size];
+						for( var x in column_size ) {
+							if ( current_size === column_size[x][0] ) {
+								next_size = column_size[x];
+							}
+						}
+
+						size_count += next_size[2];
 
 						if ( size_count > 1 || i === 0 ) {
 
@@ -700,7 +705,7 @@ jQuery( function( $ ) {
 								current_field.val( current_content );
 							}
 
-							size_count = column_sizes[current_size];
+							size_count = next_size[2];
 						} else if ( current_parents.is( '.axisbuilder-first-column' ) ) {
 							current_parents.removeClass( 'axisbuilder-first-column' );
 							current_content = current_content.replace( ' first', '' );

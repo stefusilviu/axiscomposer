8ea3fc61cc191edeaa25ddcf495d1c3a40ea961a
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-04-03 09:13:14 +0545

    sidebar backbone model header

--- includes/admin/views/html-admin-page-sidebars.php
+++ includes/admin/views/html-admin-page-sidebars.php
@@ -23,8 +23,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<div class="axisbuilder-backbone-modal-content modal-animation">
 			<section class="axisbuilder-backbone-modal-main" role="main">
 				<header class="axisbuilder-backbone-modal-header">
-					<a class="modal-close modal-close-link" href="#"><span class="close-icon"><span class="screen-reader-text">Close media panel</span></span></a>
-					<h1><?php _e( 'Last warning, are you sure?', 'axisbuilder' ); ?></h1>
+					<h1 class="modal-title"><?php _e( 'Last warning, are you sure?', 'axisbuilder' ); ?></h1>
+					<button class="modal-close dashicons dashicons-no-alt">
+						<span class="screen-reader-text">Close Overlay</span>
+					</button>
 				</header>
 				<article class="axisbuilder-backbone-modal-article">
 					<form action="" method="post">

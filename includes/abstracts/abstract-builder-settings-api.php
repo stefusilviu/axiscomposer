c399ee1e6f18eb7885221edcbfcda77c18f9a906
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-04-30 21:14:17 +0545

    Some workflow

--- includes/abstracts/abstract-builder-settings-api.php
+++ includes/abstracts/abstract-builder-settings-api.php
@@ -12,4 +12,96 @@
  */
 abstract class AB_Settings_API {
 
+	/**
+	 * The plugin ID. Used for option names.
+	 * @var string
+	 */
+	public $plugin_id = 'axisbuilder_';
+
+	/**
+	 * Method ID.
+	 * @var string
+	 */
+	public $id = '';
+
+	/**
+	 * Method title.
+	 * @var string
+	 */
+	public $method_title = '';
+
+	/**
+	 * Method description.
+	 * @var string
+	 */
+	public $method_description = '';
+
+	/**
+	 * 'yes' if the method is enabled
+	 * @var string
+	 */
+	public $enabled;
+
+	/**
+	 * Setting values.
+	 * @var array
+	 */
+	public $settings = array();
+
+	/**
+	 * Form option fields.
+	 * @var array
+	 */
+	public $form_fields = array();
+
+	/**
+	 * Validation errors.
+	 * @var array
+	 */
+	public $errors = array();
+
+	/**
+	 * Sanitized fields after validation.
+	 * @var array
+	 */
+	public $sanitized_fields = array();
+
+	/**
+	 * Admin Options
+	 *
+	 * Setup the gateway settings screen.
+	 * Override this in your gateway.
+	 *
+	 * @since 1.0.0
+	 */
+	public function admin_options() { ?>
+
+		<h3><?php echo ( ! empty( $this->method_title ) ) ? $this->method_title : __( 'Settings', 'woocommerce' ) ; ?></h3>
+
+		<?php echo ( ! empty( $this->method_description ) ) ? wpautop( $this->method_description ) : ''; ?>
+
+		<table class="form-table">
+			<?php //$this->generate_settings_html(); ?>
+		</table><?php
+	}
+
+	/**
+	 * Initialise Settings Form Fields
+	 *
+	 * Add an array of fields to be displayed
+	 * on the gateway's settings screen.
+	 *
+	 * @since 1.0.0
+	 * @return string
+	 */
+	public function init_form_fields() {}
+
+	/**
+	 * Get the form fields after they are initialized
+	 *
+	 * @return array of options
+	 */
+	public function get_form_fields() {
+		return apply_filters( 'axisbuilder_settings_api_form_fields_' . $this->id, $this->form_fields );
+	}
 }

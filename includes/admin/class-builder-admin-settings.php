f6b1e36cbd1deb519f6fa696a80a263b0854e314
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-05-14 22:25:26 +0545

    Revert - Dismissible notice for settings page

--- includes/admin/class-builder-admin-settings.php
+++ includes/admin/class-builder-admin-settings.php
@@ -89,11 +89,11 @@ class AB_Admin_Settings {
 	public static function show_messages() {
 		if ( sizeof( self::$errors ) > 0 ) {
 			foreach ( self::$errors as $error ) {
-				echo '<div id="message" class="error notice is-dismissible"><p><strong>' . esc_html( $error ) . '</strong></p></div>';
+				echo '<div id="message" class="error"><p><strong>' . esc_html( $error ) . '</strong></p></div>';
 			}
 		} elseif ( sizeof( self::$messages ) > 0 ) {
 			foreach ( self::$messages as $message ) {
-				echo '<div id="message" class="updated notice is-dismissible"><p><strong>' . esc_html( $message ) . '</strong></p></div>';
+				echo '<div id="message" class="updated"><p><strong>' . esc_html( $message ) . '</strong></p></div>';
 			}
 		}
 	}

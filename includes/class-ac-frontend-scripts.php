64d6f913ec90a441ad56d0f0bbf932d0f4bcfa27
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-05-27 06:45:35 +0545

    Assets modification

--- includes/class-ac-frontend-scripts.php
+++ includes/class-ac-frontend-scripts.php
@@ -19,13 +19,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 class AC_Frontend_Scripts {
 
 	/**
-	 * Contains an array of script handles registered by AB
+	 * Contains an array of script handles registered by AC
 	 * @var array
 	 */
 	private static $scripts = array();
 
 	/**
-	 * Contains an array of script handles localized by AB
+	 * Contains an array of script handles localized by AC
 	 * @var array
 	 */
 	private static $wp_localize_scripts = array();
@@ -45,23 +45,23 @@ class AC_Frontend_Scripts {
 	 * @return array
 	 */
 	public static function get_styles() {
-		return apply_filters( 'axisbuilder_enqueue_styles', array(
-			'axisbuilder-layout' => array(
-				'src'     => str_replace( array( 'http:', 'https:' ), '', AB()->plugin_url() ) . '/assets/css/axisbuilder-layout.css',
+		return apply_filters( 'axiscomposer_enqueue_styles', array(
+			'axiscomposer-layout' => array(
+				'src'     => str_replace( array( 'http:', 'https:' ), '', AB()->plugin_url() ) . '/assets/css/axiscomposer-layout.css',
 				'deps'    => '',
-				'version' => AB_VERSION,
+				'version' => AC_VERSION,
 				'media'   => 'all'
 			),
-			'axisbuilder-smallscreen' => array(
-				'src'     => str_replace( array( 'http:', 'https:' ), '', AB()->plugin_url() ) . '/assets/css/axisbuilder-smallscreen.css',
-				'deps'    => 'axisbuilder-layout',
-				'version' => AB_VERSION,
-				'media'   => 'only screen and (max-width: ' . apply_filters( 'axisbuilder_style_smallscreen_breakpoint', $breakpoint = '768px' ) . ')'
+			'axiscomposer-smallscreen' => array(
+				'src'     => str_replace( array( 'http:', 'https:' ), '', AB()->plugin_url() ) . '/assets/css/axiscomposer-smallscreen.css',
+				'deps'    => 'axiscomposer-layout',
+				'version' => AC_VERSION,
+				'media'   => 'only screen and (max-width: ' . apply_filters( 'axiscomposer_style_smallscreen_breakpoint', $breakpoint = '768px' ) . ')'
 			),
-			'axisbuilder-general' => array(
-				'src'     => str_replace( array( 'http:', 'https:' ), '', AB()->plugin_url() ) . '/assets/css/axisbuilder.css',
+			'axiscomposer-general' => array(
+				'src'     => str_replace( array( 'http:', 'https:' ), '', AB()->plugin_url() ) . '/assets/css/axiscomposer.css',
 				'deps'    => '',
-				'version' => AB_VERSION,
+				'version' => AC_VERSION,
 				'media'   => 'all'
 			),
 		) );
@@ -78,7 +78,7 @@ class AC_Frontend_Scripts {
 	 * @param  string   $version   [description]
 	 * @param  boolean  $in_footer [description]
 	 */
-	private static function register_script( $handle, $path, $deps = array( 'jquery' ), $version = AB_VERSION, $in_footer = true ) {
+	private static function register_script( $handle, $path, $deps = array( 'jquery' ), $version = AC_VERSION, $in_footer = true ) {
 		self::$scripts[] = $handle;
 		wp_register_script( $handle, $path, $deps, $version, $in_footer );
 	}
@@ -94,7 +94,7 @@ class AC_Frontend_Scripts {
 	 * @param  string   $version   [description]
 	 * @param  boolean  $in_footer [description]
 	 */
-	private static function enqueue_script( $handle, $path = '', $deps = array( 'jquery' ), $version = AB_VERSION, $in_footer = true ) {
+	private static function enqueue_script( $handle, $path = '', $deps = array( 'jquery' ), $version = AC_VERSION, $in_footer = true ) {
 		if ( ! in_array( $handle, self::$scripts ) && $path ) {
 			self::register_script( $handle, $path, $deps, $version, $in_footer );
 		}
@@ -114,7 +114,7 @@ class AC_Frontend_Scripts {
 		self::register_script( 'jquery-blockui', $assets_path . 'scripts/jquery-blockui/jquery.blockUI' . $suffix . '.js', array( 'jquery' ), '2.70' );
 
 		// Global frontend scripts
-		self::enqueue_script( 'axisbuilder', $frontend_script_path . 'axisbuilder' . $suffix . '.js', array( 'jquery', 'jquery-blockui' ) );
+		self::enqueue_script( 'axiscomposer', $frontend_script_path . 'axiscomposer' . $suffix . '.js', array( 'jquery', 'jquery-blockui' ) );
 
 		// CSS Styles
 		if ( $enqueue_styles = self::get_styles() ) {
@@ -125,7 +125,7 @@ class AC_Frontend_Scripts {
 	}
 
 	/**
-	 * Localize a AB script once.
+	 * Localize a AC script once.
 	 * @access private
 	 * @since  1.0.0 this needs less wp_script_is() calls due to https://core.trac.wordpress.org/ticket/28404 being added in WP 4.0.
 	 * @param  string $handle
@@ -147,27 +147,27 @@ class AC_Frontend_Scripts {
 	private static function get_script_data( $handle ) {
 
 		switch ( $handle ) {
-			case 'axisbuilder' :
+			case 'axiscomposer' :
 				return array(
-					'ajax_url'    => AB()->ajax_url(),
-					'ab_ajax_url' => AB_AJAX::get_endpoint(),
+					'ajax_url'    => AC()->ajax_url(),
+					'ac_ajax_url' => AC_AJAX::get_endpoint(),
 				);
 			break;
-			case 'axisbuilder-enhanced-select' :
+			case 'axiscomposer-enhanced-select' :
 				return array(
-					'i18n_select_option_text'   => esc_attr__( 'Select an option&hellip;', 'axisbuilder' ),
-					'i18n_matches_1'            => _x( 'One result is available, press enter to select it.', 'enhanced select', 'axisbuilder' ),
-					'i18n_matches_n'            => _x( '%qty% results are available, use up and down arrow keys to navigate.', 'enhanced select', 'axisbuilder' ),
-					'i18n_no_matches'           => _x( 'No matches found', 'enhanced select', 'axisbuilder' ),
-					'i18n_ajax_error'           => _x( 'Loading failed', 'enhanced select', 'axisbuilder' ),
-					'i18n_input_too_short_1'    => _x( 'Please enter 1 or more characters', 'enhanced select', 'axisbuilder' ),
-					'i18n_input_too_short_n'    => _x( 'Please enter %qty% or more characters', 'enhanced select', 'axisbuilder' ),
-					'i18n_input_too_long_1'     => _x( 'Please delete 1 character', 'enhanced select', 'axisbuilder' ),
-					'i18n_input_too_long_n'     => _x( 'Please delete %qty% characters', 'enhanced select', 'axisbuilder' ),
-					'i18n_selection_too_long_1' => _x( 'You can only select 1 item', 'enhanced select', 'axisbuilder' ),
-					'i18n_selection_too_long_n' => _x( 'You can only select %qty% items', 'enhanced select', 'axisbuilder' ),
-					'i18n_load_more'            => _x( 'Loading more results&hellip;', 'enhanced select', 'axisbuilder' ),
-					'i18n_searching'            => _x( 'Searching&hellip;', 'enhanced select', 'axisbuilder' )
+					'i18n_select_option_text'   => esc_attr__( 'Select an option&hellip;', 'axiscomposer' ),
+					'i18n_matches_1'            => _x( 'One result is available, press enter to select it.', 'enhanced select', 'axiscomposer' ),
+					'i18n_matches_n'            => _x( '%qty% results are available, use up and down arrow keys to navigate.', 'enhanced select', 'axiscomposer' ),
+					'i18n_no_matches'           => _x( 'No matches found', 'enhanced select', 'axiscomposer' ),
+					'i18n_ajax_error'           => _x( 'Loading failed', 'enhanced select', 'axiscomposer' ),
+					'i18n_input_too_short_1'    => _x( 'Please enter 1 or more characters', 'enhanced select', 'axiscomposer' ),
+					'i18n_input_too_short_n'    => _x( 'Please enter %qty% or more characters', 'enhanced select', 'axiscomposer' ),
+					'i18n_input_too_long_1'     => _x( 'Please delete 1 character', 'enhanced select', 'axiscomposer' ),
+					'i18n_input_too_long_n'     => _x( 'Please delete %qty% characters', 'enhanced select', 'axiscomposer' ),
+					'i18n_selection_too_long_1' => _x( 'You can only select 1 item', 'enhanced select', 'axiscomposer' ),
+					'i18n_selection_too_long_n' => _x( 'You can only select %qty% items', 'enhanced select', 'axiscomposer' ),
+					'i18n_load_more'            => _x( 'Loading more results&hellip;', 'enhanced select', 'axiscomposer' ),
+					'i18n_searching'            => _x( 'Searching&hellip;', 'enhanced select', 'axiscomposer' )
 				);
 			break;
 		}

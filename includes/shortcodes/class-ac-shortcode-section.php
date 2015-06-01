cc8d0f33a47408e26f41d3a38388f0aca69173ab
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-06-01 15:04:50 +0545

    Special border with ac-

--- includes/shortcodes/class-ac-shortcode-section.php
+++ includes/shortcodes/class-ac-shortcode-section.php
@@ -362,7 +362,7 @@ class AC_Shortcode_Section extends AC_Shortcode {
 		// Set Extra arrow element
 		if ( strpos( $bottom_border, 'border-extra' ) !== false ) {
 			$arrow_bg = empty( $background_color ) ? apply_filters( 'axiscomposer_background_color', '#fff' ) : $background_color;
-			self::$section_close = '<div class="axisbuilder-extra-border-element ' . $bottom_border . '"><div class="arrow-wrap"><div class="arrow-inner" style="background-color: ' . $arrow_bg . '"></div></div></div>';
+			self::$section_close = '<div class="ac-extra-border-element ' . $bottom_border . '"><div class="arrow-wrap"><div class="arrow-inner" style="background-color: ' . $arrow_bg . '"></div></div></div>';
 		} else {
 			self::$section_close = '';
 		}

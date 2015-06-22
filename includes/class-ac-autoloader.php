568720956f76fa3553656bb2814cafb4a5fdb6ae
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-06-22 14:54:19 +0545

    Removed redundant strtolower()

--- includes/class-ac-autoloader.php
+++ includes/class-ac-autoloader.php
@@ -43,7 +43,7 @@ class AC_Autoloader {
 	 * @return string
 	 */
 	private function get_file_name_from_class( $class ) {
-        return 'class-' . str_replace( '_', '-', strtolower( $class ) ) . '.php';
+        return 'class-' . str_replace( '_', '-', $class ) . '.php';
     }
 
 	/**

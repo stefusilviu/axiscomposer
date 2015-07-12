d8fbad9dd6d3f22cef3bd101003a8345434de88e
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-07-13 00:12:54 +0545

    No need in admin custom iconfont page

--- includes/admin/class-ac-admin-iconfont.php
+++ includes/admin/class-ac-admin-iconfont.php
@@ -24,41 +24,4 @@ class AC_Admin_Iconfont {
 	public static function output() {
 		include_once( 'views/html-admin-page-iconfont.php' );
 	}
-
-	/**
-	 * Returns the iconfonts to show in admin.
-	 */
-	public static function get_iconfonts() {
-		$fonts = get_option( 'axiscomposer_custom_iconfonts' );
-
-		foreach ( $fonts as $iconfont => $info ) {
-			$chars = $charmap = array();
-			$title = ( 'Fontawesome' == $iconfont ) ? __( 'Default Icons', 'axiscomposer' ) : esc_html( ucfirst( $iconfont ) );
-
-			if ( ! empty( $chars ) ) {
-				$charmap = array_merge( $charmap, $chars );
-				foreach ( $charmap as $chars ) {
-					$iconfont_lists = '';
-					$iconfont_count = count( $chars );
-					foreach ( $chars as $char ) {
-						$iconfont_lists .= '<li title="' . $char . '"><i class="' . $iconfont . '-' . $char . '"></i><label class="iconfont">' . $char . '</label></li>';
-					}
-				}
-			}
-
-			if ( ! empty( $charmap ) ) {
-				$output  = '<div id="' . esc_attr( strtolower( $iconfont ) ) . '" class="metabox-holder iconfont-wrap">';
-				$output .= '<div class="postbox">';
-				$output .= '<h3 class="iconfont-title"><strong>' . $title . '</strong> &mdash; <span class="iconfont-count count-' . $iconfont_count . '">' . number_format_i18n( $iconfont_count ) . '</span></h3>';
-				$output .= '<a class="modal-close modal-close-link del-iconfont" href="#" data-delete="' . $iconfont . '"><span class="close-icon"><span class="screen-reader-text">Close media panel</span></span></a>';
-				$output .= '<div class="inside">';
-				$output .= '<div class="icons-container">';
-				$output .= '<ul class="iconfont-lists icon">';
-				$output .= $iconfont_lists;
-				$output .= '</ul>';
-				$output .= '</div></div></div></div>';
-				echo $output;
-			}
-		}
-	}
 }
--- includes/admin/views/html-admin-page-iconfont.php
+++ includes/admin/views/html-admin-page-iconfont.php
@@ -16,19 +16,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</a> &nbsp;<span class="spinner"></span>
 	</h2>
 	<div id="msg"></div>
-	<?php if ( is_array( get_option( 'axiscomposer_custom_iconfonts' ) ) ) : ?>
-	<!-- <div class="metabox-holder meta-search">
-		<div class="postbox">
-			<h3>
-				<input class="search-icon" type="search" placeholder="Search" />
-				<span class="search-count"></span>
-			</h3>
-		</div>
-	</div> -->
-	<?php self::get_iconfonts(); ?>
-	<?php else: ?>
-	<div class="error">
-		<p><?php _e( 'No font icons uploaded. Upload some font icons to display here.', 'axiscomposer' ); ?></p>
-	</div>
-	<?php endif; ?>
 </div>

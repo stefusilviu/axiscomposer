2fd8076637a9434828b1c7c4c97d922dafb3c44b
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-08-16 09:51:22 +0545

    Introduced the new shortcode functions file

---
+++ includes/functions-ac-shortcode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * AxisComposer Shortcode Functions
+ *
+ * Functions for the shortcode system.
+ *
+ * @author   AxisThemes
+ * @category Core
+ * @package  AxisComposer/Functions
+ * @version  1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}

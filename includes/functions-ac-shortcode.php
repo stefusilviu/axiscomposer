3666c3b4a45ba46b6684f203c70564f8036eb77e
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-08-16 23:36:41 +0545

    Introduced ac_shortcode_string, added message to deprecate ac_shortcode_data, #90

--- includes/functions-ac-shortcode.php
+++ includes/functions-ac-shortcode.php
@@ -15,7 +15,48 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Generate a string from shortcode parameters.
+ * @param  string $tag
+ * @param  array  $attrs
+ * @param  string $content
+ * @param  string $type
+ * @return string
+ */
+function ac_shortcode_string( $tag, $attrs = array(), $content = '', $type = 'closed' ) {
+	$_text = '[' . $tag;
+
+	// Parse shortcode attributes.
+	if ( is_array( $attrs ) ) {
+		foreach ( $attrs as $name => $value ) {
+			if ( is_numeric( $name ) ) {
+				$_text .= ' ' . $value;
+			} else {
+				$_text .= ' ' . $name . '="' . $value . '"';
+			}
+		}
+	}
+
+	// Close the opening tag.
+	if ( 'single' === $type ) {
+		return $_text . ']';
+	} elseif ( 'self-closing' === $type ) {
+		return $_text . ' /]';
+	}
+
+	// Complete the opening tag.
+	$_text .= ']';
+
+	if ( $content ) {
+		$_text .= "\n" . trim( stripslashes( $content ) ) . "\n";
+	}
+
+	// Add the closing tag.
+	return $_text . '[/' . $tag . ']';
+}
+
+/**
  * Create a new shortcode data programmatically.
+ * @deprecated 1.0 Deprecated in favour of ac_shortcode_string.
  */
 function ac_shortcode_data( $name, $content = null, $args = array() ) {
 	$_shortcode = '[' . $name;

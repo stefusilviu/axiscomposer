40eb5312ce159995fa18c76eefc891f02910186d
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-08-16 10:55:11 +0545

    Move ac_shortcode_data funtion to seperate file

--- includes/functions-ac-shortcode.php
+++ includes/functions-ac-shortcode.php
@@ -13,3 +13,43 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+/**
+ * Create a new shortcode data programmatically.
+ */
+function ac_shortcode_data( $name, $content = null, $args = array() ) {
+	$_shortcode = '[' . $name;
+
+	if ( is_array( $args ) ) {
+		foreach ( $args as $key => $arg ) {
+			if ( is_numeric( $key ) ) {
+				$_shortcode .= ' ' . $arg;
+			} else {
+				if ( ( strpos( $arg, "'" ) === false ) && ( strpos( $arg, '&#039;' ) === false ) ) {
+					$_shortcode .= " " . $key . "='" . $arg . "'";
+				} else {
+					$_shortcode .= ' ' . $key . '="' . $arg . '"';
+				}
+			}
+		}
+	}
+
+	$_shortcode .= ']';
+
+	if ( ! is_null( $content ) ) {
+		// Strip-slashes and trim the content
+		$content = "\n" . trim( stripslashes( $content ) ) . "\n"; // Testdrive: add htmlentities()
+
+		// If the content is empty without tabs and line breaks remove it completely
+		if ( trim( $content ) == '' ) {
+			$content = '';
+		}
+
+		$_shortcode .= $content . '[/' . $name . ']';
+	}
+
+	$_shortcode .= "\n\n";
+	// $_shortcode = str_replace( '\n', '', $_shortcode );
+
+	return $_shortcode;
+}

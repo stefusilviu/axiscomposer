5fc2f9fbc6b7da02f270febbc122151195d56734
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-08-19 20:20:15 +0545

    Get scrutinizer ocular and run it on travis.sh

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -17,4 +17,9 @@ elif [ $1 == 'after' ]; then
 
 	# send coverage data to coveralls
 	php vendor/bin/coveralls --verbose --exclude-no-stmt
+
+	# get scrutinizer ocular and run it
+	wget https://scrutinizer-ci.com/ocular.phar
+	ocular.phar code-coverage:upload --format=php-clover ./tmp/clover.xml
+
 fi

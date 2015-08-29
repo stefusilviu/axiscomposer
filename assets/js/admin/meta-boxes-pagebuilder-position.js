4eb713da9e15ad59415a33f50248e4314168497a
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-08-29 07:44:52 +0545

    Fix - Adjusts the pagebuilder to the first position on smallscreen, Closes #10

--- assets/js/admin/meta-boxes-pagebuilder-position.js
+++ assets/js/admin/meta-boxes-pagebuilder-position.js
@@ -1,16 +1,18 @@
 /* global pagenow */
 jQuery( window ).load( function() {
 
-	// Page Builder at the first position
+	/**
+	 * Page Builder at the first position.
+	 */
 	jQuery( function() {
 		var builder = jQuery( '#axiscomposer-pagebuilder' ),
-			metabox = jQuery( '#normal-sortables' ),
-			postbox = metabox.find( '.postbox' );
+			columns = jQuery( '#post-body' ).hasClass( 'columns-1' ),
+			maximum = Math.max( window.innerWidth, document.documentElement.clientWidth ),
+			metabox = jQuery( '#' + ( maximum && maximum <= 850 || columns ? 'side' : 'normal' ) + '-sortables' );
 
-		if ( builder.length && ( postbox.index( builder ) !== 0 ) ) {
+		// Adjusts the pagebuilder to the fist position and triggers postbox saving.
+		if ( builder.length && metabox.find( '.postbox' ).index( builder ) !== 0 ) {
 			builder.prependTo( metabox );
-
-			// Re-save the postbox Order
 			window.postboxes.save_order( pagenow );
 		}
 	});

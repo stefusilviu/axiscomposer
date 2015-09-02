07f2912f6aed085d6945e7d3b88b0f9303f776e8
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-09-02 19:25:50 +0545

    Refactor pagebuilder position

--- assets/js/admin/meta-boxes-pagebuilder-position.js
+++ assets/js/admin/meta-boxes-pagebuilder-position.js
@@ -1,19 +1,32 @@
 /* global pagenow */
-jQuery( window ).load( function() {
+jQuery( function( $ ) {
 
 	/**
-	 * Page Builder at the first position.
+	 * Page Builder Position
 	 */
-	jQuery( function() {
-		var builder = jQuery( '#axiscomposer-pagebuilder' ),
-			columns = jQuery( '#post-body' ).hasClass( 'columns-1' ),
-			maximum = Math.max( window.innerWidth, document.documentElement.clientWidth ),
-			metabox = jQuery( '#' + ( maximum && maximum <= 850 || columns ? 'side' : 'normal' ) + '-sortables' );
+	var ac_meta_box_pagebuilder_position = {
 
-		// Adjusts the pagebuilder to the fist position and triggers postbox saving.
-		if ( builder.length && metabox.find( '.postbox' ).index( builder ) !== 0 ) {
-			builder.prependTo( metabox );
-			window.postboxes.save_order( pagenow );
+		init: function() {
+			var self = this;
+
+			$( window ).load( function() {
+				self.position();
+				window.postboxes.save_order( pagenow );
+			});
+		},
+
+		position: function() {
+			var builder = $( '#axiscomposer-pagebuilder' ),
+				columns = $( '#post-body' ).hasClass( 'columns-1' ),
+				maximum = Math.max( window.innerWidth, document.documentElement.clientWidth ),
+				metabox = $( '#' + ( maximum && maximum <= 850 || columns ? 'side' : 'normal' ) + '-sortables' );
+
+			// Prepend pagebuilder meta-box to first position
+			if ( builder.length && metabox.find( '.postbox' ).index( builder ) !== 0 ) {
+				builder.prependTo( metabox );
+			}
 		}
-	});
+	};
+
+	ac_meta_box_pagebuilder_position.init();
 });

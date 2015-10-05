ce3402edd5f7026fa8b6d921c2b07fce6bc1fb0a
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-10-05 22:02:37 +0545

    Tweak and link product permalink settings to WordPress settings

--- includes/admin/class-ac-admin-permalink-settings.php
+++ includes/admin/class-ac-admin-permalink-settings.php
@@ -15,7 +15,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * AC_Admin_Permalink_Settings Class
- * @todo Refactor the Permalink Settings Class Smartly xD
  */
 class AC_Admin_Permalink_Settings {
 
@@ -32,7 +31,7 @@ class AC_Admin_Permalink_Settings {
 	 */
 	public function settings_init() {
 		// Add a section to the permalinks page
-		add_settings_section( 'axiscomposer-permalink', __( 'Portfolio permalink base', 'axiscomposer' ), array( $this, 'settings' ), 'permalink' );
+		add_settings_section( 'axiscomposer-permalink', __( 'Portfolio Permalinks', 'axiscomposer' ), array( $this, 'settings' ), 'permalink' );
 
 		// Add our settings
 		add_settings_field(
@@ -75,9 +74,9 @@ class AC_Admin_Permalink_Settings {
 	 * Show the settings.
 	 */
 	public function settings() {
-		echo wpautop( __( 'These settings control the permalinks used for portfolio. These settings only apply when <strong>not using "default" permalinks above</strong>.', 'axiscomposer' ) );
+		echo wpautop( __( 'These settings control the permalinks specifically used for portfolio.', 'axiscomposer' ) );
 
-		$permalinks = get_option( 'axiscomposer_permalinks' );
+		$permalinks          = get_option( 'axiscomposer_permalinks' );
 		$portfolio_permalink = $permalinks['portfolio_base'];
 
 		// Get base slug
@@ -86,28 +85,23 @@ class AC_Admin_Permalink_Settings {
 
 		$structures = array(
 			0 => '',
-			1 => '/' . trailingslashit( $portfolio_base ),
-			2 => '/' . trailingslashit( $base_slug ),
-			3 => '/' . trailingslashit( $base_slug ) . trailingslashit( '%portfolio_cat%' )
+			1 => '/' . trailingslashit( $base_slug ),
+			2 => '/' . trailingslashit( $base_slug ) . trailingslashit( '%portfolio_cat%' )
 		);
 		?>
-		<table class="form-table">
+		<table class="form-table ac-permalink-structure">
 			<tbody>
 				<tr>
-					<th><label><input name="portfolio_permalink" type="radio" value="<?php echo $structures[0]; ?>" class="actog" <?php checked( $structures[0], $portfolio_permalink ); ?> /> <?php _e( 'Default', 'axiscomposer' ); ?></label></th>
-					<td><code><?php echo home_url(); ?>/?portfolio=sample-portfolio</code></td>
+					<th><label><input name="portfolio_permalink" type="radio" value="<?php echo esc_attr( $structures[0] ); ?>" class="actog" <?php checked( $structures[0], $portfolio_permalink ); ?> /> <?php _e( 'Default', 'axiscomposer' ); ?></label></th>
+					<td><code class="default-example"><?php echo esc_html( home_url() ); ?>/?portfolio=sample-portfolio</code> <code class="non-default-example"><?php echo esc_html( home_url() ); ?>/<?php echo esc_html( $portfolio_base ); ?>/sample-portfolio/</code></td>
 				</tr>
 				<tr>
-					<th><label><input name="portfolio_permalink" type="radio" value="<?php echo $structures[1]; ?>" class="actog" <?php checked( $structures[1], $portfolio_permalink ); ?> /> <?php _e( 'Portfolio', 'axiscomposer' ); ?></label></th>
-					<td><code><?php echo home_url(); ?>/<?php echo $portfolio_base; ?>/sample-portfolio/</code></td>
+					<th><label><input name="portfolio_permalink" type="radio" value="<?php echo esc_attr( $structures[1] ); ?>" class="actog" <?php checked( $structures[1], $portfolio_permalink ); ?> /> <?php _e( 'Project base', 'axiscomposer' ); ?></label></th>
+					<td><code><?php echo esc_html( home_url() ); ?>/<?php echo esc_html( $base_slug ); ?>/sample-portfolio/</code></td>
 				</tr>
 				<tr>
-					<th><label><input name="portfolio_permalink" type="radio" value="<?php echo $structures[2]; ?>" class="actog" <?php checked( $structures[2], $portfolio_permalink ); ?> /> <?php _e( 'Project base', 'axiscomposer' ); ?></label></th>
-					<td><code><?php echo home_url(); ?>/<?php echo $base_slug; ?>/sample-portfolio/</code></td>
-				</tr>
-				<tr>
-					<th><label><input name="portfolio_permalink" type="radio" value="<?php echo $structures[3]; ?>" class="actog" <?php checked( $structures[3], $portfolio_permalink ); ?> /> <?php _e( 'Category base Project', 'axiscomposer' ); ?></label></th>
-					<td><code><?php echo home_url(); ?>/<?php echo $base_slug; ?>/portfolio-category/sample-portfolio/</code></td>
+					<th><label><input name="portfolio_permalink" type="radio" value="<?php echo esc_attr( $structures[2] ); ?>" class="actog" <?php checked( $structures[2], $portfolio_permalink ); ?> /> <?php _e( 'Project based category', 'axiscomposer' ); ?></label></th>
+					<td><code><?php echo esc_html( home_url() ); ?>/<?php echo esc_html( $base_slug ); ?>/portfolio-category/sample-portfolio/</code></td>
 				</tr>
 				<tr>
 					<th><label><input name="portfolio_permalink" id="axiscomposer_custom_selection" type="radio" value="custom" class="tog" <?php checked( in_array( $portfolio_permalink, $structures ), false ); ?> />
@@ -123,7 +117,18 @@ class AC_Admin_Permalink_Settings {
 				jQuery('input.actog').change(function() {
 					jQuery( '#axiscomposer_permalink_structure' ).val( jQuery( this ).val() );
 				});
-
+				jQuery('.permalink-structure input').change(function() {
+					jQuery('.ac-permalink-structure').find('code.non-default-example, code.default-example').hide();
+					if ( jQuery(this).val() ) {
+						jQuery('.ac-permalink-structure code.non-default-example').show();
+						jQuery('.ac-permalink-structure input').removeAttr('disabled');
+					} else {
+						jQuery('.ac-permalink-structure code.default-example').show();
+						jQuery('.ac-permalink-structure input:eq(0)').click();
+						jQuery('.ac-permalink-structure input').attr('disabled', 'disabled');
+					}
+				});
+				jQuery('.permalink-structure input:checked').change();
 				jQuery( '#axiscomposer_permalink_structure' ).focus( function(){
 					jQuery( '#axiscomposer_custom_selection' ).click();
 				});
@@ -146,8 +151,7 @@ class AC_Admin_Permalink_Settings {
 			// Cat and tag bases
 			$axiscomposer_portfolio_category_slug = ac_clean( $_POST['axiscomposer_portfolio_category_slug'] );
 			$axiscomposer_portfolio_tag_slug      = ac_clean( $_POST['axiscomposer_portfolio_tag_slug'] );
-
-			$permalinks = get_option( 'axiscomposer_permalinks' );
+			$permalinks                           = get_option( 'axiscomposer_permalinks' );
 
 			if ( ! $permalinks ) {
 				$permalinks = array();
@@ -159,7 +163,7 @@ class AC_Admin_Permalink_Settings {
 			// Portfolio base
 			$portfolio_permalink = ac_clean( $_POST['portfolio_permalink'] );
 
-			if ( $portfolio_permalink == 'custom' ) {
+			if ( 'custom' === $portfolio_permalink ) {
 				// Get permalink without slashes
 				$portfolio_permalink = trim( ac_clean( $_POST['portfolio_permalink_structure'] ), '/' );
 

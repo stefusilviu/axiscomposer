a3229851a4c930f053b49b58188fbae6bf57ff82
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-10-20 23:30:23 +0545

    Fix travis-ci builds with wp-cli test suite installer

--- tests/bin/install.sh
+++ tests/bin/install.sh
@@ -15,8 +15,6 @@ WP_VERSION=${5-latest}
 WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
 WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
 
-set -ex
-
 download() {
 	if [ `which curl` ]; then
 		curl -s "$1" > "$2";
@@ -24,6 +22,22 @@ download() {
 		wget -nv -O "$2" "$1"
 	fi
 }
+
+if [[ $WP_VERSION =~ [0-9]+\.[0-9]+(\.[0-9]+)? ]]; then
+	WP_TESTS_TAG="tags/$WP_VERSION"
+else
+	# http serves a single offer, whereas https serves multiple. we only want one
+	download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
+	grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
+	LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
+	if [[ -z "$LATEST_VERSION" ]]; then
+		echo "Latest WordPress version could not be found"
+		exit 1
+	fi
+	WP_TESTS_TAG="tags/$LATEST_VERSION"
+fi
+
+set -ex
 
 install_wp() {
 	if [ -d $WP_CORE_DIR ]; then
@@ -56,7 +70,7 @@ install_test_suite() {
 	if [ ! -d $WP_TESTS_DIR ]; then
 		# set up testing suite
 		mkdir -p $WP_TESTS_DIR
-		svn co --quiet https://develop.svn.wordpress.org/trunk/tests/phpunit/includes/ $WP_TESTS_DIR/includes
+		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
 	fi
 
 	cd $WP_TESTS_DIR

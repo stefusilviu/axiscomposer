0705661963ff316e7c60f6406d4b5a81ed771de9
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-10-23 22:11:36 +0545

    Tweak docs block

--- includes/admin/functions-ac-admin.php
+++ includes/admin/functions-ac-admin.php
@@ -62,7 +62,8 @@ function axiscomposer_update_options( $options ) {
 
 /**
  * Get a setting from the settings API.
- * @param  mixed $option_name
+ * @param  mixed  $option_name
+ * @param  string $default
  * @return string
  */
 function axiscomposer_settings_get_option( $option_name, $default = '' ) {

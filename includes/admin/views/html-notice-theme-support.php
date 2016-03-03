4c7d2a51cd7bf46574ece9e335ab65459c35214e
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-03-03 15:56:02 +0545

    Blank target for theme support notice links

--- includes/admin/views/html-notice-theme-support.php
+++ includes/admin/views/html-notice-theme-support.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <div id="message" class="updated axiscomposer-message ac-connect">
-	<p><?php printf( __( '<strong>Your theme does not declare AxisComposer support</strong> &#8211; Please read our %sintegration%s guide or check out our %sofficial themes%s which are designed specifically for use with AxisComposer.', 'axiscomposer' ), '<a href="' . esc_url( apply_filters( 'axiscomposer_docs_url', 'http://docs.axisthemes.com/document/third-party-custom-theme-compatibility/', 'theme-compatibility' ) ) . '">', '</a>', '<a href="' . esc_url( 'http://www.axisthemes.com/themes/' ) . '">', '</a>' ); ?></p>
+	<p><?php printf( __( '<strong>Your theme does not declare AxisComposer support</strong> &#8211; Please read our %sintegration%s guide or check out our %sofficial themes%s which are designed specifically for use with AxisComposer.', 'axiscomposer' ), '<a target="_blank" href="' . esc_url( apply_filters( 'axiscomposer_docs_url', 'http://docs.axisthemes.com/document/third-party-custom-theme-compatibility/', 'theme-compatibility' ) ) . '">', '</a>', '<a target="_blank" href="' . esc_url( 'http://www.axisthemes.com/themes/' ) . '">', '</a>' ); ?></p>
 	<p class="submit">
 		<a href="http://www.axisthemes.com/themes" class="button-primary" target="_blank"><?php _e( 'Official Themes', 'axiscomposer' ); ?></a>
 		<a href="<?php echo esc_url( apply_filters( 'axiscomposer_docs_url', 'http://docs.axisthemes.com/document/plugins/axiscomposer/third-party-custom-theme-compatibility/', 'theme-compatibility' ) ); ?>" class="button-secondary" target="_blank"><?php _e( 'Theme Integration Guide', 'axiscomposer' ); ?></a>

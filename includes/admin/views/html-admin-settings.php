3574758cc1087e9118f9cbc129b9e24f8f7efd31
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-03-10 18:52:51 +0545

    Change heading and tab markup

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -10,9 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="wrap axiscomposer">
 	<form method="<?php echo esc_attr( apply_filters( 'axiscomposer_settings_form_method_tab_' . $current_tab, 'post' ) ); ?>" id="mainform" action="" enctype="multipart/form-data">
-		<div class="icon32 icon32-axiscomposer-settings" id="icon-axiscomposer"><br /></div>
-		<h1><?php printf( __( '%s Settings', 'axiscomposer' ), esc_html( $tabs[ $current_tab ] ) ); ?></h1>
-		<h2 class="nav-tab-wrapper axis-nav-tab-wrapper">
+		<nav class="nav-tab-wrapper axis-nav-tab-wrapper">
 			<?php
 				foreach ( $tabs as $name => $label ) {
 					echo '<a href="' . admin_url( 'admin.php?page=ac-settings&tab=' . $name ) . '" class="nav-tab ' . ( $current_tab == $name ? 'nav-tab-active' : '' ) . '">' . $label . '</a>';
@@ -20,7 +18,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 				do_action( 'axiscomposer_settings_tabs' );
 			?>
-		</h2>
+		</nav>
+		<h1 class="screen-reader-text"><?php echo esc_html( $tabs[ $current_tab ] ); ?></h1>
 		<?php
 			do_action( 'axiscomposer_sections_' . $current_tab );
 

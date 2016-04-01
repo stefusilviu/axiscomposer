986da8538d906f5868dbb5bca7af6db02805f5cf
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-04-01 10:38:28 +0545

    Tweak init tooltips

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -1,18 +1,21 @@
 jQuery( function ( $ ) {
 
 	// Tooltips
-	var tiptip_args = {
-		'attribute': 'data-tip',
-		'fadeIn': 50,
-		'fadeOut': 50,
-		'delay': 200
-	};
-	$( '.tips, .help_tip, .axiscomposer-help-tip' ).tipTip( tiptip_args );
+	$( document.body ).on( 'init_tooltips', function() {
+		var tiptip_args = {
+			'attribute': 'data-tip',
+			'fadeIn': 50,
+			'fadeOut': 50,
+			'delay': 200
+		};
 
-	// Add tiptip to parent element for widefat tables
-	$( '.parent-tips' ).each( function() {
-		$( this ).closest( 'a, th' ).attr( 'data-tip', $( this ).data( 'tip' ) ).tipTip( tiptip_args ).css( 'cursor', 'help' );
-	});
+		$( '.tips, .help_tip, .axiscomposer-help-tip' ).tipTip( tiptip_args );
+
+		// Add tiptip to parent element for widefat tables
+		$( '.parent-tips' ).each( function() {
+			$( this ).closest( 'a, th' ).attr( 'data-tip', $( this ).data( 'tip' ) ).tipTip( tiptip_args ).css( 'cursor', 'help' );
+		});
+	}).trigger( 'init_tooltips' );
 
 	// Select availability
 	$( 'select.availability' ).change( function() {

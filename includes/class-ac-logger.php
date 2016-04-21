fe920c81faf367640b59d8f2dcefc4af969cd763
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-04-22 01:46:11 +0545

    Close file if it's already open before clear

--- includes/class-ac-logger.php
+++ includes/class-ac-logger.php
@@ -20,6 +20,7 @@ class AC_Logger {
 
 	/**
 	 * Stores open file _handles.
+	 *
 	 * @var array
 	 * @access private
 	 */
@@ -37,22 +38,26 @@ class AC_Logger {
 	 */
 	public function __destruct() {
 		foreach ( $this->_handles as $handle ) {
-			@fclose( $handle );
+			if ( is_resource( $handle ) ) {
+				fclose( $handle );
+			}
 		}
 	}
 
 	/**
 	 * Open log file for writing.
+	 *
 	 * @access private
-	 * @param  mixed $handle
-	 * @return bool  success
+	 * @param  string $handle
+	 * @param  string $mode
+	 * @return bool   success
 	 */
-	private function open( $handle ) {
+	protected function open( $handle, $mode = 'a' ) {
 		if ( isset( $this->_handles[ $handle ] ) ) {
 			return true;
 		}
 
-		if ( $this->_handles[ $handle ] = @fopen( ac_get_log_file_path( $handle ), 'a' ) ) {
+		if ( $this->_handles[ $handle ] = @fopen( ac_get_log_file_path( $handle ), $mode ) ) {
 			return true;
 		}
 
@@ -60,28 +65,66 @@ class AC_Logger {
 	}
 
 	/**
+	 * Close a handle.
+	 *
+	 * @param  string $handle
+	 * @return bool   success
+	 */
+	protected function close( $handle ) {
+		$result = false;
+
+		if ( is_resource( $this->_handles[ $handle ] ) ) {
+			$result = fclose( $this->_handles[ $handle ] );
+			unset( $this->_handles[ $handle ] );
+		}
+
+		return $result;
+	}
+
+	/**
 	 * Add a log entry to chosen file.
+	 *
 	 * @param string $handle
 	 * @param string $message
+	 *
+	 * @return bool
 	 */
 	public function add( $handle, $message ) {
+		$result = false;
+
 		if ( $this->open( $handle ) && is_resource( $this->_handles[ $handle ] ) ) {
-			$time = date_i18n( 'm-d-Y @ H:i:s -' ); // Grab Time
-			@fwrite( $this->_handles[ $handle ], $time . " " . $message . "\n" );
+			$time   = date_i18n( 'm-d-Y @ H:i:s -' ); // Grab Time
+			$result = fwrite( $this->_handles[ $handle ], $time . " " . $message . "\n" );
 		}
 
 		do_action( 'axiscomposer_log_add', $handle, $message );
+
+		return false !== $result;
 	}
 
 	/**
 	 * Clear entries from chosen file.
-	 * @param mixed $handle
+	 *
+	 * @param string $handle
+	 *
+	 * @return bool
 	 */
 	public function clear( $handle ) {
-		if ( $this->open( $handle ) && is_resource( $this->_handles[ $handle ] ) ) {
-			@ftruncate( $this->_handles[ $handle ], 0 );
+		$result = false;
+
+		// Close the file if it's already open.
+		$this->close( $handle );
+
+		/**
+		 * $this->open( $handle, 'w' ) == Open the file for writing only. Place the file pointer at the beginning of the file,
+		 * and truncate the file to zero length.
+		 */
+		if ( $this->open( $handle, 'w' ) && is_resource( $this->_handles[ $handle ] ) ) {
+			$result = true;
 		}
 
 		do_action( 'axiscomposer_log_clear', $handle );
+
+		return $result;
 	}
 }
